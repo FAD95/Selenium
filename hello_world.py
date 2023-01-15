ff708ab31573030fde81eaa77cb217d2d258c697
FAD95 <feescorciam@protonmail.com>
2023-01-14 23:14:13 -0500

    Code working

--- hello_world.py
+++ hello_world.py
@@ -1,11 +1,13 @@
 import unittest
 from pyunitreport import HTMLTestRunner
+from selenium.webdriver.chrome.service import Service
 from selenium import webdriver
 
 class HelloWorld(unittest.TestCase):
     
     def setUp(self):
-        self.driver = webdriver.Chrome(executable_path = r'/usr/bin/chromedriver')
+        chrome = Service(r"/usr/bin/chromedriver")
+        self.driver = webdriver.Chrome(service=chrome)
         driver = self.driver
         driver.implicitly_wait(10)
 
@@ -16,8 +18,8 @@ class HelloWorld(unittest.TestCase):
         # def test_visit_wikipedia(self):
         #     self.driver.get('http://www.wikipedia.org')
     
-    #def tearDown(self):
-    #    self.driver.quit()
+    def tearDown(self):
+       self.driver.quit()
 
 if __name__ == '__main__':
     unittest.main(verbosity = 2, testRunner = HTMLTestRunner(output = 'reportes', report_name = 'hello-world-report'))
