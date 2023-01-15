9720e58d9b23148dd39345abe22c4dfe360d9cf1
FAD95 <feescorciam@protonmail.com>
2023-01-15 00:42:31 -0500

    Add classmethod

--- hello_world.py
+++ hello_world.py
@@ -5,21 +5,23 @@ from selenium import webdriver
 
 class HelloWorld(unittest.TestCase):
     
-    def setUp(self):
+    @classmethod
+    def setUpClass(cls):
         chrome = Service(r"/usr/bin/chromedriver")
-        self.driver = webdriver.Chrome(service=chrome)
-        driver = self.driver
+        cls.driver = webdriver.Chrome(service=chrome)
+        driver = cls.driver
         driver.implicitly_wait(10)
 
     def test_hello_world(self):
         driver = self.driver
         driver.get('http://www.platzi.com')
     
-        # def test_visit_wikipedia(self):
-        #     self.driver.get('http://www.wikipedia.org')
+    def test_visit_wikipedia(self):
+        self.driver.get('http://www.wikipedia.org')
     
-    def tearDown(self):
-       self.driver.quit()
+    @classmethod
+    def tearDownClass(cls):
+        cls.driver.quit()
 
 if __name__ == '__main__':
     unittest.main(verbosity = 2, testRunner = HTMLTestRunner(output = 'reportes', report_name = 'hello-world-report'))
